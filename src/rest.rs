e8274118979dfe109dd20296d34351ce7f452ae7
Tom Black <tom.black06007@gmail.com>
2023-12-14 21:49:34 -0700

    added the add acronym handler function. Tested to make sure it writes to the db.

--- src/rest.rs
+++ src/rest.rs
@@ -7,11 +7,12 @@ use sqlx::MySqlPool;
 
 pub fn acronym_service() -> Router {
     Router::new()
-    .route("/", get(get_all_acronyms))
-    .route("/:id", get(get_acronym_by_id))
+    .route("/", get(get_all_acronyms_handler))
+    .route("/:id", get(get_acronym_by_id_handler))
+    .route("/add", post(add_acronym_handler))
 }
 
-async fn get_all_acronyms(Extension(conn): Extension<MySqlPool>) -> Result<Json<Vec<Acronym>>, StatusCode> {
+async fn get_all_acronyms_handler(Extension(conn): Extension<MySqlPool>) -> Result<Json<Vec<Acronym>>, StatusCode> {
     if let Ok(acronyms) = all_acronyms(&conn).await {
         Ok(Json(acronyms))
     } else {
@@ -19,9 +20,17 @@ async fn get_all_acronyms(Extension(conn): Extension<MySqlPool>) -> Result<Json<
     }
 }
 
-async fn get_acronym_by_id(Extension(conn):Extension<MySqlPool>, Path(id): Path<i32>) -> Result<Json<Acronym>, StatusCode>{
+async fn get_acronym_by_id_handler(Extension(conn):Extension<MySqlPool>, Path(id): Path<i32>) -> Result<Json<Acronym>, StatusCode>{
     if let Ok(acronym) = acronym_by_id(&conn, id).await {
         Ok(Json(acronym))
+    } else {
+        Err(StatusCode::SERVICE_UNAVAILABLE)
+    }
+}
+
+async fn add_acronym_handler(Extension(conn):Extension<MySqlPool>, extract::Json(acronym): extract::Json<Acronym>) -> Result<Json<i32>, StatusCode> {
+    if let Ok(new_id) = crate::db::add_acronym(&conn, acronym.acronym, acronym.definition).await {
+        Ok(Json(new_id))
     } else {
         Err(StatusCode::SERVICE_UNAVAILABLE)
     }
